586e4494d13e6156fcf968aa7a4cb60c2c9f5603
Yui <ytkakentan@yahoo.co.jp>
2020-10-09 22:14:54 +0900

    changed to ==

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ let answer = [2];
 // forループで１から５までの数値で、答えの2が当たるまで推測を促すpromptを表示させる。
 // ヒント１：if文とも組み合わせて、推測が当たった場合はalertで'Good job! See what happens next :)'を表示。当たったらゲーム終了にする。
 // ヒント２：外れたらpromptで'Ops! Maybe, another try?'のメッセージとともに記入欄を表示。
-let numbers = [1, 3, 4, 5];
+let numbers = [1, 3, 4, 5 ];
 
-for (let i = 0 ; i <= 5 ; i++) { //変数iの役割；候補１　配列のインデックス番号　候補２　繰り返し処理の回数　→候補２を選択
+for (let i = 0 ; i <= 4 ; i++) { //変数iの役割；候補１　配列のインデックス番号　候補２　繰り返し処理の回数　→候補２を選択
     // 何をどうしたいかは後でかく 1.if文とも組み合わせて、推測が当たった場合はalertで'Good job! See what happens next :)'を表示。当たったらゲーム終了にする。2.外れたらpromptで'Ops! Maybe, another try?'のメッセージとともに記入欄を表示。
     // if文の基本構文をまずは書く 
     // if (条件式) {
@@ -24,20 +24,20 @@ for (let i = 0 ; i <= 5 ; i++) { //変数iの役割；候補１　配列のイ�
     // } else {
     //     文；
     // }
-    if (question === answer){
-        congrats = alert('Good job! See what happens next :)');
+    if (question == answer){
+        alert('Good job! See what happens next :)');
         const img = document.getElementById('bonus-img');
-        console.log(img);
+        console.log('あたり');
         img.innerHTML = `
             <h1>You made it! 🎮</h1>
             <iframe src="https://giphy.com/embed/11sBLVxNs7v6WA" width="480" height="217" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cheer-cheering-11sBLVxNs7v6WA"></a></p>
         `;
         break;
     } else if (
-        question === numbers[0] ||
-        question === numbers[1] ||
-        question === numbers[2] ||
-        question === numbers[3]
+        question == numbers[0] ||
+        question == numbers[1] ||
+        question == numbers[2] ||
+        question == numbers[3]
     ) {
         question = prompt('Ops! Maybe, another try?');
         console.log('はずれ');
